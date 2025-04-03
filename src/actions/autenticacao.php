2eeb9a72cd4f6aa78c7ec53ffea1c71abc9d3857
Killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-04-03 13:37:58 -0300

    separe logic

--- src/actions/autenticacao.php
+++ src/actions/autenticacao.php
@@ -1,5 +1,4 @@
 <?php
-  error_reporting(E_ALL ^ E_WARNING);
   require_once("conexao.php");
   $sql = "SELECT * FROM usuario";
 
@@ -14,27 +13,13 @@
   
   if(password_verify($_REQUEST['senha'], $usuario['senha'])){
     echo "<script type='javascript'>alert('Autenticacao efetuada com sucesso!');";
+    header('location', 'index.php');
   }
 
   if ($_REQUEST['senha'] && password_verify($_REQUEST['senha'], $usuario['senha']) == false){
     echo "<script>alert('Autenticacao Malsucedida!');</script>";
+    header('location', 'index.php');
   }
 
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MYSQL</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
-</head>
-<body>
-  <form action="autenticacao.php" method="POST">
-      <input type="email" required name="email" placeholder="E-mail">
-      <input type="password" required name="senha" placeholder="Senha">
-      <input type="submit">
-  </form>
-</body>
-</html>
