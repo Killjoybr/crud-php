5b5504630bda2918064d31b4b6c5a323b1dd972e
Killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-04-03 17:59:09 -0300

    separe logic from form view

--- src/actions/editar.php
+++ src/actions/editar.php
@@ -1,6 +1,5 @@
 <?php
-  error_reporting(E_ALL ^ E_WARNING);
-  include_once 'conexao.php';
+  include_once '../conexao.php';
 
   $cargos = $conexao->query("SELECT * FROM usuario_cargo");
 
@@ -11,44 +10,17 @@
   $cargo = $_REQUEST['cargo'] ?: NULL;
 
   
-  if ($id && $nome && $email && $senha && $cargo) {
-    $sql = "UPDATE usuario SET nome = :nome, email = :email, senha = :senha, cargo = :cargo WHERE id = :id";
-    $statement = $conexao->prepare($sql);
-    $statement->bindParam(':nome',$nome);
-    $statement->bindParam(':email',$email);
-    $statement->bindParam(':senha',$senha);
-    $statement->bindParam(':cargo',$cargo);
-    $statement->bindParam(':id',$id);
+  $sql = "UPDATE usuario SET nome = :nome, email = :email, senha = :senha, cargo = :cargo WHERE id = :id";
+  $statement = $conexao->prepare($sql);
+  $statement->bindParam(':nome',$nome);
+  $statement->bindParam(':email',$email);
+  $statement->bindParam(':senha',$senha);
+  $statement->bindParam(':cargo',$cargo);
+  $statement->bindParam(':id',$id);
 
-    $statement->execute();
+  $statement->execute();
 
-    header('Location: index.php');
-  }
+  header('Location: ../../index.php');
+  
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-  <head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
-  </head>
-  <body>
-<form method="post" action="editar.php?id=<?php echo $id?>">
-      <h2>Atualização de Cadastro</h2>
-      <input type="text" placeholder="Nome" name="nome" required>
-      <br><br>
-      <input type="email" placeholder="jão@email.com" name="email" required>
-      <br><br>
-      <input type="password" placeholder="senha" name="senha" required>
-      <br><br>
-      <select name="cargo">
-        <option></option>
-        <?php  foreach ($cargos as $cargo): ?>
-        <option label=<?= $cargo['Descricao'] ?>></option>
-            <?= $cargo['id'] ?>
-          </option>
-        <?php endforeach; ?>
-      </select>
-      <input type="submit" value="Atualizar" name="atualizar">
-    </form>
-  </body>
-</html>
