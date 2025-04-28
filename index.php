9c0826f8706705ad40e811da791881d794529ad4
Killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-04-28 16:32:40 -0300

    add startbootstrap theme

--- index.php
+++ index.php
@@ -1,87 +1,102 @@
-<?php
-    // while ($row = $usuarios->fetch(PDO::FETCH_OBJ)) {
-    //     echo '
-    //         <tr>
-    //             <td>TESTE</td>
-    //             <td>TESTE</td>
-    //             <td>TESTE</td>
-    //             <td>TESTE</td>
-    //         </tr>
-    //     ';
-    // };
- 
-    // include("conexao.php");
-    include_once("./src/conexao.php");
-
-    $getUsers = "SELECT * FROM usuario";
-    $getCargos = "SELECT * FROM usuario_cargo";
-
-    $usuarios = $conexao->query($getUsers);
-    $cargos = $conexao->query($getCargos);
-    $cargosArr = []; 
-
-    foreach ($cargos as $cargo){
-      $cargosArr[] = $cargo['Descricao'];
-    }
-
-    $cargosCombo = $conexao->query($getCargos);
-?>
-
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MYSQL</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css"> -->
-    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.min.css"> -->
-  </head>
-    <body>
-      <a href="./src/autenticacao.html">Login</a>
-      <table>
-        <caption><h1>Tabela de Usuarios<strong></caption>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>E-mail</th>
-            <th>Senha</th>
-            <th>Cargo</th>
-            <th>Ações</th>
-          </tr>
-        </thead>
-        <?php  foreach ($usuarios as $usuario):?>
-          <tr>
-            <td><?= $usuario['id'] ?></td>
-            <td><?= $usuario['nome'] ?></td>
-            <td><?= $usuario['email'] ?></td>
-            <td><?= $usuario['senha'] ?></td>
-            <td><?= empty($cargosArr[$usuario['cargo'] - 1]) ? NULL : $cargosArr[$usuario['cargo'] - 1] ?></td>
-            <td>
-              <a href="./src/excluir.php?id=<?= $usuario['id'] ?>"> <i class="fa fa-trash" style="padding-right: 10px;"></i> </a>
-              <a href="./src/editar.php?id=<?= $usuario['id'] ?>"> <i class="fa fa-pencil"></i> </a>
-            </td>                   
-          </tr>
-        <?php endforeach; ?>
-      </table>
-      <form method="post" action="./src/cadastro.php">
-      <h2>Cadastro de Usuário</h2>
-      <input type="text" placeholder="Jõaozinho da Goiaba" name="nome" required>
-      <br><br>
-      <input type="email" placeholder="jão@email.com" name="email" required>
-      <br><br>
-      <input type="password" placeholder="senha" name="senha" required>
-      <br><br>
-        <select name="cargo">
-          <option></option>
-          <?php  foreach ($cargosCombo as $cargo): ?>
-          <option label=<?= $cargo['Descricao'] ?>><?= $cargo['id'] ?></option>
-          <?php endforeach; ?>
-          </select>
-        <br><br>
-        <input type="submit" value="Cadastrar" name="cadastrar">
-      </form>
-    </body>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Bem vindo(a)!</h1>
+                                    </div>
+                                    <form class="user" method="POST" action="./src/actions/autenticacao.php">
+                                        <div class="form-group">
+                                            <input type="email" class="form-control form-control-user"
+                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                placeholder="Digite seu e-mail..." name="email" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="exampleInputPassword" placeholder="Senha" name="senha" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <label class="custom-control-label" for="customCheck">Remember
+                                                    Me</label>
+                                            </div>
+                                        </div>
+                                        <input type="submit" class="btn btn-primary btn-user btn-block" value="Logar">
+                                        <hr>
+                                        <a href="index.html" class="btn btn-google btn-user btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Entre com Google
+                                        </a>
+                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i> Entre com Facebook
+                                        </a>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Esqueceu a senha?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="register.html">Ainda não tem uma conta? Cadastre-se!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
 </html>
+
