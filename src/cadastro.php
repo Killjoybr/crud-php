fcf42f463d0a0192901584effa7ba723fd8aa87b
Killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-04-30 14:53:26 -0300

    update sql and business logic

--- src/cadastro.php
+++ src/cadastro.php
@@ -1,22 +1,20 @@
 <?php
-  require_once 'conexao.php';
+  require_once($_SERVER['DOCUMENT_ROOT'].'/projects/crud-php/config/conexao.php');
   
-  $usuario = $_REQUEST['nome'];
+  $nome = $_REQUEST['nome'];
   $email = $_REQUEST['email'];
   $senha = $_REQUEST['senha'];
-  $cargo = $_REQUEST['cargo'] ? $_REQUEST['cargo'] : NULL;
-  
+
   $hash = password_hash($senha, PASSWORD_BCRYPT);
 
-  $sql = "INSERT INTO usuario (nome, email, senha, cargo) VALUES (:usuario, :email, :senha, :cargo)";
+  $sql = "INSERT INTO usuario (nome, email, senha) VALUES (:nome, :email, :senha)";
 
   $statement = $conexao->prepare($sql);
-  $statement->bindParam(':usuario',$usuario);
+  $statement->bindParam(':nome',$nome);
   $statement->bindParam(':email',$email);
   $statement->bindParam(':senha',$hash);
-  $statement->bindParam(':cargo',$cargo);
 
   $statement->execute();
 
-  header('Location: ../index.php');
+  header('Location: ../index.php?message=Cadastro efetuado com sucesso!');
 ?>
