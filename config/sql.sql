a0b8b1e8a6018bf722e893439909e10292312362
Killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-06-05 18:21:12 -0300

    fix code conventions

--- config/sql.sql
+++ config/sql.sql
@@ -2,16 +2,16 @@ create database if not exists desenvolvimento;
 
 use desenvolvimento;
 
-create table if not exists usuario(
-	id int auto_increment not null primary key,
-	nome varchar(150) not null,
-	email varchar(150) not null,
-	senha varchar(100) not null
+create table if not exists usuario (
+    id int auto_increment not null primary key,
+    nome varchar(150) not null,
+    email varchar(150) not null,
+    senha varchar(100) not null
 );
 
-create table if not exists usuario_cargo(
-	id int auto_increment not null primary key,
-	descricao varchar(100) not null
+create table if not exists usuario_cargo (
+    id int auto_increment not null primary key,
+    descricao varchar(100) not null
 );
 
 alter table usuario
