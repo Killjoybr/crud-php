19c6cfcd90723ce09e0cf91a854d26996ca07e21
Killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-06-05 18:05:50 -0300

    add session logic

--- src/tabela.php
+++ src/tabela.php
@@ -1,16 +1,19 @@
-<?php 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/projects/crud-php/config/conexao.php');
+<?php
+  session_start();
+  NULL !== $_SESSION['email'] ? NULL : header('location: ../index.php?mensagem=Acesso negado!'); 
 
-$getUsuarios = 'SELECT * FROM usuario';
-$getCargos = 'SELECT * FROM usuario_cargo';
+  require_once($_SERVER['DOCUMENT_ROOT'] . '/projects/crud-php/config/conexao.php');
 
-$usuarios = $conexao->query($getUsuarios);
-$cargos = $conexao->query($getCargos);
-$cargosArr = [];
+  $getUsuarios = 'SELECT * FROM usuario';
+  $getCargos = 'SELECT * FROM usuario_cargo';
 
-foreach($cargos as $cargo){
-  $cargosArr[] = $cargo['descricao'];
-}
+  $usuarios = $conexao->query($getUsuarios);
+  $cargos = $conexao->query($getCargos);
+  $cargosArr = [];
+
+  foreach($cargos as $cargo){
+    $cargosArr[] = $cargo['descricao'];
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -342,7 +345,7 @@ foreach($cargos as $cargo){
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?=$_SESSION['usuario']?></span>
                                 <img class="img-profile rounded-circle"
                                     src="img/undraw_profile.svg">
                             </a>
