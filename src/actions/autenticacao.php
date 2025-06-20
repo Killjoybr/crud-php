90e345cca44f289f2ad784c05f667ce97cd653ba
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-06-20 02:12:58 -0300

    update auth logic

--- src/actions/autenticacao.php
+++ src/actions/autenticacao.php
@@ -3,7 +3,12 @@
   $sql = "SELECT * FROM usuario";
 
   $usuarios = $conexao->query($sql);
-  $autenticacao = "SELECT senha, nome FROM usuario where email = :email";
+  #$autenticacao = "SELECT senha, nome, tipo FROM usuario where email = :email";
+
+  $autenticacao = "SELECT u.senha, u.nome, u.usuario_tipo, c.descricao 
+                   FROM usuario as u 
+                   LEFT JOIN usuario_tipo as c ON u.usuario_tipo = c.id 
+                   WHERE u.email = :email";
 
   $statement = $conexao->prepare($autenticacao);
   $statement->bindParam('email', $_REQUEST['email']);
@@ -16,9 +21,15 @@
 
     $_SESSION['email'] = $_REQUEST['email'];
     $_SESSION['usuario'] = $usuario['nome'];
+    $_SESSION['tipo'] = $usuario['usuario_tipo'];
 
+    $url = match($usuario['tipo']){
+      'admin' => '/projects/crud-php/src/views/administrador.php',
+      'colaborador' => '/projects/crud-php/src/views/colaborador.php',
+      default => '/projects/crud-php/src/views/index.html'
+    };
 
-    header('location: ../tabela.php');
+    header("location: $url");
   }
 
   if ($_REQUEST['senha'] && password_verify($_REQUEST['senha'], $usuario['senha']) == false){
