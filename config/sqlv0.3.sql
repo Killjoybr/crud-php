3bceb311adbc97eb180aa3b683f8181504a2c86c
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-06-21 22:57:54 -0300

    alter default usuario_tipo

---
+++ config/sqlv0.3.sql
@@ -0,0 +1,4 @@
+-- -----------------------------------------------------
+-- update Table `usuario`
+-- -----------------------------------------------------
+ALTER TABLE `usuario` ALTER COLUMN `usuario_tipo`SET DEFAULT 1;
