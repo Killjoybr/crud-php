44698aafd197e2480ec5fc354a724f99e1cd5057
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-06-22 16:27:24 -0300

    update modularization

---
+++ src/componentes/scripts.php
@@ -0,0 +1,16 @@
+<!-- Bootstrap core JavaScript-->
+<script src="/projects/crud-php/vendor/jquery/jquery.min.js"></script>
+<script src="/projects/crud-php/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="/projects/crud-php/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="/projects/crud-php/js/sb-admin-2.min.js"></script>
+
+<!-- Page level plugins -->
+<script src="/projects/crud-php/vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="/projects/crud-php/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="/projects/crud-php/js/demo/datatables-demo.js"></script>
